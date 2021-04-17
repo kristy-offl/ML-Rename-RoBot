6906feffceb9d4bd1645b7d1b3da77c2cfdbd809
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2021-04-17 19:39:30 +0530

    #Str

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -29,9 +29,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 async def start(bot, update):
           await bot.send_message(
           chat_id=update.chat.id,
-          text=Scripted.START_TEXT,
           parse_mode="html",
           disable_web_page_preview=True,
+          text=Scripted.START_TEXT.format(update.from_user.first_name),
           reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='Mirror Group', url=f'https://t.me/{Config.MIRROR_GROUP}') ],
                                                [ InlineKeyboardButton(text='🚀Updates Channel🚀', url=f'https://t.me/{Config.UPDATE_CHANNEL}'),
                                                  InlineKeyboardButton(text='🚨Support Group🚨', url=f'https://t.me/{Config.UPDATE_GROUP}') ],
